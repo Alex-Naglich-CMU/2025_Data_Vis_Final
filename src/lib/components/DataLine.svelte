055a14a1eb11d1a0ba2c7b6ce929631f8980b482
Kim Credit <kcredit@andrew.cmu.edu>
2025-11-12 10:05:08 -0500

    fixed colors

--- src/lib/components/DataLine.svelte
+++ src/lib/components/DataLine.svelte
@@ -3,6 +3,14 @@
 	import type { ScaleTime, ScaleLinear } from 'd3';
 	import type { PlottableAveragePrice, PlottablePricePoint } from '$lib/scripts/drug-types';
 
+	const colors = {
+		red: '#9A2F1F',
+		blue: '#54707C',
+		orange: '#DF7C39',
+		tan: '#BFA97F',
+		cream: '#F6F5EC'
+	}
+
 	const {
 		data = [],
 		dataType,
@@ -18,7 +26,6 @@
 	const lineGenerator = $derived(
 		d3
 			.line<any>() // Use 'any' here due to the mixed input types
-			.curve(d3.curveStepAfter)
 			.x((d) => xScale(d.date))
 			.y((d) => yScale(dataType === 'mean' ? d.averagePrice : d.price))
 	);
@@ -27,7 +34,7 @@
 
 	// Styling Placeholder
 	const strokeColor = $derived(
-		dataType === 'mean' ? 'red' : dataType === 'brand' ? 'blue' : 'green'
+		dataType === 'mean' ? colors.red : dataType === 'brand' ? colors.blue : colors.orange
 	);
 	const strokeWidth = $derived(dataType === 'mean' ? 3 : dataType === 'brand' ? 5 : 2);
 	const opacity = $derived(dataType === 'mean' ? 1.0 : dataType === 'brand' ? 0.7 : 0.5);
