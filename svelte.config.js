335ba7c1276f9372ebf1739da5aa01656cb30e9f
Alex Naglich <alexnaglich@gmail.com>
2025-11-13 10:32:02 -0500

    Oopsy adding this back to the svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 
+const isDev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -7,7 +9,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.BASE_PATH || ''
+			base: isDev ? '' : '/2025_Data_Vis_Final'
 		}
 	}
 };
