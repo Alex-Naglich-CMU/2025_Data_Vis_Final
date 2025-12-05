8961d61c80693475db68b851dac5925efa25ca85
Alex Naglich <alexnaglich@gmail.com>
2025-12-05 13:25:04 -0500

    Told vite to stop watching the prices folder. Might stop the insane vite hotload issue Anissa was having.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		watch: {
+			// Ignore the large prices data folder to prevent unnecessary HMR triggers
+			ignored: ['**/src/lib/data/prices/**']
+		}
+	}
 });
